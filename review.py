f26c5ac432de74a6cb67aa0870d2c5a1ddbefeed
olgaaverina <olyaaverina.s@gmail.com>
2023-01-10 13:14:18 +0300

    добавлены permissions

--- review.py
+++ review.py
@@ -1,85 +1,134 @@
-def authorize_order(client: (str, int), items: [(str, int)]): -> dict:
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+def authorize_order(client: (str, int), items: [(str, int)]) -> dict:
     # Абстрактное решение тут
     person: Person = get_person(*client)
     checked_items = {i: person.can_buy(get_item(i)) for i in items}
     return checked_items
-    
 
-def get_person_from_db(person_type, person_id) :
-    ...    
-                
-def get_item_from_db(item_type, item_id) :
+
+def get_person_from_db(person_type: str, person_id: int):
     ...
-    
-def get_recipt_from_db(person_id, item_id):
-	  ...
-
-    
-def get_person(name, id):
-    if name == 'doctor':
-        return Doctor(name, id, get_person_from_db(name, id).speciality_id)
-    if name == 'client':
-        return Client(name, id)
-
-class Person:
-    def __init__(id):
-        self.id = id
-  
-class Client(Person):
-    def __init__(id: int):
-        super().__init__(id)
-    
-    def is_reciept_exist(item_id):
-  	return get_recipt_from_db(self.id, item_id) is not None
-
-    def can_buy(item: Item) -> bool:
-        if isinstance(item) == CommonItem:
-    	    return true
-        if isinstance(item) == SpecialItem:
-    	    return false
-        if isinstance(item) == RecieptItem:
-    	    return is_reciept_exist(item.id)
-  	  
-class Doctor(Person):
-    def __init__(id: int, speciality_id: int):
-  	super().__init__(id)
-	self.speciality_id = speciality_id
-    
-    def is_spicially_allowed(item):
-	return self.speciality_id == item.speciality_id
-    
-    def can_buy(item: Item) -> bool:
-  	if isinstance(item) == CommonItem or isinstance(item) == RecieptItem:
-    	    return true
-        if isinstance(item) == SpecialItem:
-    	    return is_spicially_allowed()
 
 
-def get_item(name, id):
-    if name == 'common_items':
-  	return CommonItem(id)
-    if name == 'special_items':
-  	return SpecialItem(id, get_item_from_db(item.name, item.id).speciality_id)
+def get_item_from_db(item_type: str, item_id: int):
+    ...
+
+
+def get_reciept_from_db(person_id: int, item_id: int):
+    ...
+
+
+class PersonTypeEnum(Enum, str):
+    doctor = "doctor"
+    client = "client"
+
+
+class ItemTypeEnum(Enum, str):
+    special_item = "special_item"
+    receipt_item = "receipt_item"
+    common_item = "common_item"
+
+
+def is_recipe_exist(person_id: int, item_id: int) -> bool:
+    return get_reciept_from_db(person_id, item_id) is not None
+
+
+def is_specially_allowed(person, item):
+    return get_person_from_db(*person).speciality_id == item.speciality_id
+
+
+class Permission(ABC):
+
+    @abstractmethod
+    def can_buy(self, person_id, item) -> bool:
+        pass
+
+
+class ClientPermission(Permission):
+    def can_buy(self, person_id, item) -> bool:
+        if isinstance(item, CommonItem):
+            return True
+        if isinstance(item, SpecialItem):
+            return False
+        if isinstance(item, RecieptItem):
+            return is_recipe_exist(person_id, item.id)
+
+
+class DoctorPermission(Permission):
+    def can_buy(self, person_id, item) -> bool:
+        if isinstance(item, (CommonItem, RecieptItem)):
+            return True
+        if isinstance(item, SpecialItem):
+            return is_specially_allowed((PersonTypeEnum.doctor, person_id), item)
+
+
+def get_person(person_type: str, person_id: int):
+    persons = {PersonTypeEnum.doctor: Doctor,
+               PersonTypeEnum.client: Client}
+    return persons[PersonTypeEnum(person_type)](person_id)
+
+
+def get_item(item_name, item_id):
+    items = {ItemTypeEnum.common_item: CommonItem,
+             ItemTypeEnum.special_item: SpecialItem,
+             ItemTypeEnum.receipt_item: RecieptItem}
+    return items[ItemTypeEnum(item_name)](item_id)
+
 
 class Item:
-    def __init__(id: int):
+    def __init__(self, id: int):
         self.id = id
-    
-class CommonItem(Item):
-    def __init__(id: int):
-  	super().__init__(id)
-	
-class RecieptItem(Item):
-    def __init__(id: int):
-  	super().__init__(id)
-    
-class SpecialItem(Item):
-    def __init__(id: int, speciality_id: int):
-  	super().__init__(id)
-        self.speciality_id = speciality_id
-  
 
-  
+
+class CommonItem(Item):
+    def __init__(self, id: int):
+        super().__init__(id)
+
+
+class RecieptItem(Item):
+    def __init__(self, id: int):
+        super().__init__(id)
+
+
+class SpecialItem(Item):
+    def __init__(self, id: int, speciality_id: int):
+        super().__init__(id)
+        self.speciality_id = speciality_id
+
+
+class Person(ABC):
+    def __init__(self, person_id: int):
+        self.person_id: int = person_id
+        self.permissions = Permission()
+
+    def can_buy(self, item: Item) -> bool:
+        return self.permissions.can_buy(self.person_id, item)
+
+
+class Client(Person):
+    def __init__(self, person_id: int):
+        super().__init__(person_id)
+        self.permissions = ClientPermission()
+
+
+class Doctor(Person):
+    def __init__(self, person_id: int):
+        super().__init__(person_id)
+        self.permissions = DoctorPermission()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
